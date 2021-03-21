7981646540a2c8e17d2879ee276cc078515e3eaa
skorpions2000 <dmytro.kravchenko.live@gmail.com>
2021-03-21 15:39:55 +0100

    screen manager

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.scrollview import ScrollView
 
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
+from kivymd.uix.chip import MDChip
 
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.label import MDLabel
@@ -35,15 +36,29 @@ class AskView(MDCard):
     login = StringProperty('login')
     question_content = StringProperty('ask')
     question_detail = StringProperty('ask')
-    def get_size(self):
-        return self.size_mytext
     pass
 
 
 class AnswerView(MDCard):
-
-    text = StringProperty('Answer')
+    login = StringProperty('login')
+    answer_content = StringProperty('answer')
+    answer_detail = StringProperty('answer')
     pass
+
+
+class Tags(MDChip):
+    text_tag = StringProperty('tag')
+    pass
+
+
+class TagsBlock(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(Tags(text_tag="la la la"))
+        self.add_widget(Tags(text_tag="llo lo lo"))
+        self.add_widget(Tags(text_tag="llu lu lu"))
+        self.add_widget(Tags(text_tag="la li la"))
+        self.add_widget(Tags(text_tag="lyyy la la"))
 
 
 
@@ -55,17 +70,19 @@ class Content(MDGridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_widget(AskView(login='My login', question_content="La alalla lsjdjd ufdhvdsl fgsdlkglkdsfj fdsgsdfg dsgfds gdf gdsf g sdf g sdf gd sfsdfgdsgsdfg ", question_detail='My Ask dfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddfg fds nddc'))
-        self.add_widget(AnswerView(text="text1"))
-        self.add_widget(AnswerView(text="text2"))
-        self.add_widget(AnswerView(text="text3"))
-        self.add_widget(AnswerView(text="text4"))
+        self.add_widget(AnswerView(login='My login', answer_content="La alalla lsjdjd ufdhvdsl fgsdlkglkdsfj fdsgsdfg dsgfds gdf gdsf g sdf g sdf gd sfsdfgdsgsdfg ", answer_detail='My Ask dfg f'))
+        self.add_widget(AnswerView(login='My login', answer_content="La alalla lsjdjd ufdhvdsl fgsdlkglkdsfj fdsgsdfg dsgfds gdf gdsf g sdf g sdf gd sfsdfgdsgsdfg ", answer_detail='My Askjhnljknhkjhljk lkj jlk lkh lkhlk j;l kjl jlkjj; f'))
+        self.add_widget(AnswerView(login='My login', answer_content="La alalla lsjdjd ufdhvdsl fgsdlkglkdsfj fdsgsdfg dsgfds gdf gdsf g sdf g sdf gd sfsdfgdsgsdfg ", answer_detail='My Ashhloihihi juhlk hlkhl khk hkjf'))
+        self.add_widget(AnswerView(login='My login', answer_content="La alalla lsjdjd ufdhvdsl fgsdlkglkdsfj", answer_detail='My A hjh lhjlohjlhlkjhlkj hkj hklh jklhklj hlkh khl khkh kljh lk'))
+        self.add_widget(AnswerView(login='My login', answer_content="La alalla lsjdjd u", answer_detail='Myddfsddf bgjhgj khj jgjh gjk gkjh gkjh gkj gjk gjk gkj kj hgjh gkj gk khjjhhk'))
+
 
 
 class Example(MDApp):
     path_to_kv_file = "main.kv"
 
     def build(self):
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
         return Builder.load_string(KV)
 
     def update_kv_file(self, text):
